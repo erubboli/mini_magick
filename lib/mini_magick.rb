67e65ecbcecf0a646254fd9923eaa65fdc7a879a
dn2k <rubboli@gmail.com>
2010-12-01 20:16:23 +0100

    ignore return error (see: https://gist.github.com/5c96202241be5bc8caa3)

--- lib/mini_magick.rb
+++ lib/mini_magick.rb
@@ -348,13 +348,13 @@ module MiniMagick
         destroy!
 
         # Raise the appropriate error
-        if sub.output =~ /no decode delegate/i || sub.output =~ /did not return an image/i
-          raise Invalid, sub.output
-        else
+        # if sub.output =~ /no decode delegate/i || sub.output =~ /did not return an image/i
+        #  raise Invalid, sub.output
+        #else
           # TODO: should we do something different if the command times out ...?
           # its definitely better for logging.. otherwise we dont really know
           raise Error, "Command (#{command.inspect.gsub("\\", "")}) failed: #{{:status_code => sub.exitstatus, :output => sub.output}.inspect}"
-        end
+        #end
       else
         sub.output
       end
